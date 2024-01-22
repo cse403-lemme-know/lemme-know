7c277e5065e0656202c6e28bd8f961c74b0d5e86
Finn Bear <finnbearlabs@gmail.com>
2024-01-21 17:38:24 -0800

    Import AWS DynamoDB SDK.

--- backend/go.mod
+++ backend/go.mod
@@ -4,10 +4,11 @@ go 1.20
 
 require (
 	github.com/aws/aws-lambda-go v1.45.0
+	github.com/aws/aws-sdk-go v1.50.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.0
 )
 
 require (
-	golang.org/x/net v0.17.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
 func main() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	_ = dynamodb.New(sess)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello world!")
 	})
