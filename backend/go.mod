86d0c61959caadaa5906714bcc4ed2650c11f480
Finn Bear <finnbearlabs@gmail.com>
2024-01-22 10:38:36 -0800

    Implement simple AWS Lambda REST microservice skeleton in Go. (#1)
    * Implement simple AWS Lambda REST microservice in Go.
    
    * Import AWS DynamoDB SDK.
    
    * Import AWS API Gateway Management API SDK.

---
+++ backend/go.mod
@@ -0,0 +1,14 @@
+module backend
+
+go 1.20
+
+require (
+	github.com/aws/aws-lambda-go v1.45.0
+	github.com/aws/aws-sdk-go v1.50.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.0
+)
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+)
+
+func main() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	_ = dynamodb.New(sess)
+	_ = apigatewaymanagementapi.New(sess)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello world!")
+	})
+
+	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
+}
