12c2d4d2811a834d5d243d638b6bde8c96d155cd
Finn Bear <finnbearlabs@gmail.com>
2024-01-30 18:19:21 -0800

    Get started on Terraform configuration. (#39)
    * Lambda terraform.
    
    * IAM terraform (partial).
    
    * EventBridge terraform.
    
    * Format terraform.
    
    * API Gateway REST terraform.
    
    * Add TODO comments.

---
+++ terraform/lambda.tf
@@ -0,0 +1,73 @@
+resource "aws_lambda_function" "backend" {
+  filename         = "../backend/bin/bootstrap.zip"
+  function_name    = "lemmeknow-backend"
+  handler          = "bootstrap"
+  memory_size      = 128
+  role             = aws_iam_role.backend_role.arn
+  runtime          = "provided.al2023"
+  source_code_hash = filebase64sha256("../backend/bin/bootstrap.zip")
+  timeout          = 10
+}
+
+resource "aws_iam_role" "backend_role" {
+  name               = "lemmeknow-backend-role"
+  assume_role_policy = data.aws_iam_policy_document.backend_role.json
+
+}
+
+data "aws_iam_policy_document" "backend_role" {
+  statement {
+    sid = "1"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "backend_policy" {
+  name   = "lemmeknow-backend-policy"
+  role   = aws_iam_role.backend_role.id
+  policy = data.aws_iam_policy_document.backend_policy.json
+}
+
+data "aws_iam_policy_document" "backend_policy" {
+  statement {
+    sid = "logs"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+  #statement {
+  #  sid = "dynamodb"
+  #  actions = [
+  #    "dynamodb:DeleteItem",
+  #    "dynamodb:GetItem",
+  #    "dynamodb:PutItem",
+  #    "dynamodb:Scan",
+  #    "dynamodb:Query",
+  #    "dynamodb:UpdateItem"
+  #  ]
+  #  resources = [
+  #    "${aws_dynamodb_table.TODO_TABLE_NAME_1.arn}",
+  #    "${aws_dynamodb_table.TODO_TABLE_NAME_2.arn}"
+  #  ]
+  #}
+  #statement {
+  #  sid = "websocket"
+  #  action = [
+  #    "execute-api:*"
+  #  ]
+  #  resources = [
+  #    "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.NAME.id}/*/*/*"
+  #  ]
+  #}
+}
---
+++ terraform/s3.tf
@@ -0,0 +1,9 @@
+/*
+TODO: add the following resources.
+aws_s3_bucket
+aws_s3_bucket_acl
+aws_s3_bucket_cors_configuration
+aws_s3_bucket_website_configuration
+aws_s3_bucket_public_access_block
+aws_s3_object (for_each frontend file)
+*/
