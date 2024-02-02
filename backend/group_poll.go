8b3ec75d3760c639f037fd685aa059b99911a6ab
Finn Bear <finnbearlabs@gmail.com>
2024-02-01 17:44:38 -0800

    Implement chat and poll API's. (#64)

--- backend/group_poll.go
+++ backend/group_poll.go
@@ -21,6 +21,7 @@ type PatchPollRequest struct {
 // API's related to polls within a group.
 func RestGroupPollAPI(router *mux.Router, database Database) {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		group := r.Context().Value(GroupKey).(*Group)
 		switch r.Method {
 		case http.MethodPut:
 			var request PutPollRequest
@@ -28,8 +29,22 @@ func RestGroupPollAPI(router *mux.Router, database Database) {
 				http.Error(w, "could not decode body", http.StatusBadRequest)
 				return
 			}
-			// TODO: database
-			_ = request
+
+			var options = []PollOption{}
+
+			for _, name := range request.Options {
+				options = append(options, PollOption{Name: name, Votes: []UserID{}})
+			}
+
+			if err := database.CreatePoll(group.GroupID, Poll{
+				Title:     request.Title,
+				Timestamp: unixMillis(),
+				Options:   options,
+				DoneFlag:  false,
+			}); err != nil {
+				http.Error(w, "could not create poll", http.StatusInternalServerError)
+				return
+			}
 
 			WriteJSON(w, nil)
 		case http.MethodPatch:
@@ -43,7 +58,10 @@ func RestGroupPollAPI(router *mux.Router, database Database) {
 
 			WriteJSON(w, nil)
 		case http.MethodDelete:
-			// TODO: database
+			if err := database.DeletePoll(group.GroupID); err != nil {
+				http.Error(w, "could not delete poll", http.StatusInternalServerError)
+				return
+			}
 			WriteJSON(w, nil)
 		}
 	})
