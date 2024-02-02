1eb0cb72e148f1f3cf815e1ec85c9beffd9657c8
Brooklyn Zhu <zhubrook@uw.edu>
2024-02-02 15:44:05 -0800

    fix lint and warning

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit({
+			onwarn: (warning, handler) => {
+				if (
+					warning.message &&
+					warning.message.includes(
+						'visible, non-interactive elements with an on:click event must be accompanied by an on:keydown, on:keyup, or on:keypress event'
+					)
+				) {
+					return;
+				}
+				handler(warning);
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
