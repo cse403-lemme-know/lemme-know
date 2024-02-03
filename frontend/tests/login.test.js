102d8e0ebedd84b28a77c49c7f63019ae28b6087
Brooklyn Zhu <zhubrook@uw.edu>
2024-02-02 19:00:06 -0800

    add integration tests

--- frontend/tests/login.test.js
+++ frontend/tests/login.test.js
@@ -1,8 +1,9 @@
 import { expect, test } from '@playwright/test';
 
 test('Page has header and message', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
-    expect(await page.textContent('h1')).toBe('LemmeKnow');
-    expect(await page.textContent('h3')).toContain('SCHEDULE HANGOUTS, PLAN ROAD-TRIPS, SHARE CALENDARS, EVERYTHING, EVERYWHERE, ALL AT ONCE.');
+	await page.goto('http://localhost:5173/');
+	expect(await page.textContent('h1')).toBe('LemmeKnow');
+	expect(await page.textContent('h3')).toContain(
+		'SCHEDULE HANGOUTS, PLAN ROAD-TRIPS, SHARE CALENDARS, EVERYTHING, EVERYWHERE, ALL AT ONCE.'
+	);
 });
-
