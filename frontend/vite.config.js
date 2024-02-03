0fde177d2914a85ae8ee7d3d66f1f4c0a6c8b310
Brooklyn Zhu <zhubrook@uw.edu>
2024-02-02 16:17:51 -0800

    fix integration tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		sveltekit({
-			onwarn: (warning, handler) => {
-				if (
-					warning.message &&
-					warning.message.includes(
-						'visible, non-interactive elements with an on:click event must be accompanied by an on:keydown, on:keyup, or on:keypress event'
-					)
-				) {
-					return;
-				}
-				handler(warning);
-			}
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
