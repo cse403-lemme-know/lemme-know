b185e5d6c8b4d4f3118fb05a0e3808f6da449667
Mohamad Abdi <mabdi596@cs.washington.edu>
2024-02-06 12:36:36 -0800

    acm and cloudfront template done needs some more editing

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -7,3 +7,137 @@ aws_cloudfront_distribution
 aws_iam_policy_document
 aws_s3_bucket_policy
 */
+
+resource "aws_cloudfront_origin_access_identity" "example" {
+    comment = "origin access identity"
+}
+
+resource "aws_cloudfront_distribution" "name" {
+    origin {
+        domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+        origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+        origin_id = local.s3_origin_id
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    default_root_object = "index.html"
+
+    logging_config {
+        include_cookies = false
+        bucket = "mylogs.s3.amazonaws.com"
+        prefix = "myprefix"
+    }
+
+    aliases = ["mysite.example.com", "yoursite.example.com"]
+
+    default_cache_behavior {
+      allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+      cached_methods = ["GET", "HEAD"]
+      target_origin_id = local.s3_origin_id
+
+      forwarded_values {
+        query_string = false 
+
+        cookies {
+            forward = "none"
+        }
+      }
+
+      min_ttl = 0
+      default_ttl = 86400
+      max_ttl = 31536000
+      compress = true
+      viewer_protocol_policy = "redirect-to-https"
+    }
+
+    ordered_cache_behavior {
+      path_pattern = "/content/*"
+      allowed_methods = ["GET", "HEAD", "OPTIONS"]
+      cached_methods = ["GET", "HEAD"]
+      target_origin_id = local.s3_origin_id
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+
+      min_ttl = 0
+      default_ttl = 3600
+      max_ttl = 86400
+      compress = true
+      viewer_protocol_policy = "redirect-to-https"
+    }
+
+    price_class = "PriceClass_200"
+    restrictions {
+      geo_restriction {
+        restriction_type = "whitelist"
+        locations = ["US", "CA", "GB", "DE"]
+      }
+    }
+    
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
+}
+
+data "aws_iam_policy_document" "example" {
+    statement {
+        sid = "1"
+
+        actions = [
+            "s3:ListAllMyBuckets",
+            "s3:GetBucketLocation",
+        ]
+
+        resources = [
+            "arn:aws:s3:::*",
+        ]
+    }
+
+    statement {
+        actions = [
+            "s3:ListBucket",
+        ]
+
+        resources = [
+            "arn:aws:s3:::${var.s3_bucket_name}",
+        ]
+
+        condition {
+          test = "StringLike"
+          variable = "s3:prefix"
+
+          values = [
+            "",
+            "home/",
+            "home/&{aws:username}/",
+          ]
+        }
+    }
+
+    statement {
+        actions = [
+            "s3:*",
+        ]
+
+        resources = [
+            "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
+            "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
+        ]
+    }
+}
+
+resource "aws_iam_policy" "example" {
+    name = "example_policy"
+    path = "/"
+    policy = data.aws_iam_policy_document.example.json  
+}
+
+resource "aws_s3_bucket_policy" "example" {
+    bucket = aws_s3_bucket.example.id
+    policy = data.aws_iam_policy_document.example  
+}
