daa22de0a9543a3e0ace30243779c67d0f77b48b
abdimoh596 <107062181+abdimoh596@users.noreply.github.com>
2024-02-06 13:48:23 -0800

    Mohamad DynamoDB, ACM, and CloudFront Terraform (#63)
    * Mohamad Weekly report done
    
    * updated mohamads report
    
    * dynamodb table progress
    
    * acm and cloudfront template done needs some more editing
    
    * Prepare to merge.
    
    ---------
    
    Co-authored-by: Mohamad Abdi <mabdi596@cs.washington.edu>
    Co-authored-by: Finn Bear <finnbearone@gmail.com>

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,40 @@
-/*
-TODO: add the following resources
+resource "aws_dynamodb_table" "Group" {
+  name         = "GroupTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "GroupID"
 
-aws_dynamodb_table (x3)
-*/
+  attribute {
+    name = "GroupID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "User" {
+  name         = "UserTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserID"
+
+  attribute {
+    name = "UserID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "Message" {
+  name         = "MessageTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "MessageId"
+  range_key    = "Timestamp"
+
+  attribute {
+    name = "UserID"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "N"
+  }
+}
+
+//ignore poll/pollresult
