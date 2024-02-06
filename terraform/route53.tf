4aba67c99c6311d8aa02f27236af56ffc06030d3
Finn Bear <finnbearlabs@gmail.com>
2024-02-05 22:51:10 -0800

    Create Route53 terrarform configurations (except ACM record).

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,6 +1,17 @@
-/*
-TODO: add the following resources
+resource "aws_route53_zone" "backend" {
+  name = var.domain
+}
 
-aws_route53_zone
-aws_route53_record (x2)
+// TODO: Uncomment when Cloudfront is added
+/*
+resource "aws_route53_record" "cloudfront" {
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.backend.domain_name
+    zone_id                = aws_cloudfront_distribution.backend.hosted_zone_id
+  }
+  name    = aws_route53_zone.backend.name
+  type    = "A"
+  zone_id = aws_route53_zone.backend.zone_id
+}
 */
