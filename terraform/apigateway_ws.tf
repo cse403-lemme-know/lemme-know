11982fd7dfa04e481120498bbf4b1133236b2070
Finn Bear <finnbearlabs@gmail.com>
2024-02-07 08:58:46 -0800

    Run terraform fmt.

--- terraform/apigateway_ws.tf
+++ terraform/apigateway_ws.tf
@@ -1,149 +1,132 @@
-/*
-TODO: add the following resources.
-
-aws_iam_policy_document
-aws_iam_policy
-aws_iam_role
-aws_apigatewayv2_api
-aws_apigatewayv2_integration
-aws_apigatewayv2_integration_response
-aws_apigatewayv2_route
-aws_apigatewayv2_route_response
-aws_apigatewayv2_route (x3)
-aws_apigatewayv2_route_response (x3)
-aws_apigatewayv2_stage
-aws_lambda_permission
-*/
-
 data "aws_iam_policy_document" "backend" {
-    statement {
-        sid = "1"
+  statement {
+    sid = "1"
 
-        actions = [
-            "s3:ListAllMyBuckets",
-            "s3:GetBucketLocation",
-        ]
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
 
-        resources = [
-            "arn:aws:s3:::*",
-        ]
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+
+      values = [
+        "",
+        "home/",
+        "home/&{aws:username}/",
+      ]
     }
+  }
 
-    statement {
-        actions = [
-            "s3:ListBucket",
-        ]
+  statement {
+    actions = [
+      "s3:*",
+    ]
 
-        resources = [
-            "arn:aws:s3:::${var.s3_bucket_name}",
-        ]
-
-        condition {
-            test = "StringLike"
-            variable = "s3:prefix"
-
-            values = [
-                "",
-                "home/",
-                "home/&{aws:username}/",
-            ]
-        }
-    }
-
-    statement {
-        actions = [
-            "s3:*",
-        ]
-
-        resources = [
-            "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
-            "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
-        ]
-    }  
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
+    ]
+  }
 }
 
 resource "aws_iam_policy" "backend" {
-    name = "backend_policy"
-    path = "/"
-    policy = data.aws_iam_policy_document.backend.json  
+  name   = "backend_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.backend.json
 }
 
 resource "aws_iam_role" "backend_role" {
-    name = "backend_role"
+  name = "backend_role"
 
-    assume_role_policy = jsondecode({
-        Version = "2024-02-06"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Sid = ""
-                Principal = {
-                    Service = "ec2.amazonaws.com"
-                }
-            },
-        ]
-    })  
+  assume_role_policy = jsondecode({
+    Version = "2024-02-06"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 resource "aws_apigatewayv2_api" "backend" {
-    name = "backend-websocket-api"
-    protocol_type = "WEBSOCKET"
-    route_selection_expression = "$request.body.action" 
+  name                       = "backend-websocket-api"
+  protocol_type              = "WEBSOCKET"
+  route_selection_expression = "$request.body.action"
 }
 
 resource "aws_apigatewayv2_integration" "backend" {
-    api_id = aws_apigatewayv2_api.backend
-    integration_type = "MOCK"  
+  api_id           = aws_apigatewayv2_api.backend
+  integration_type = "MOCK"
 }
 
 resource "aws_apigatewayv2_integration_response" "backend" {
-    api_id = aws_apigatewayv2_api.backend.id
-    integration_id = aws_apigatewayv2_integration.backend.id
-    integration_response_key = "/200/"  
+  api_id                   = aws_apigatewayv2_api.backend.id
+  integration_id           = aws_apigatewayv2_integration.backend.id
+  integration_response_key = "/200/"
 }
 
 resource "aws_apigatewayv2_route" "connect" {
-    api_id = aws_apigatewayv2_api.backend.id
-    route_key = "$default"  
+  api_id    = aws_apigatewayv2_api.backend.id
+  route_key = "$default"
 }
 
 resource "aws_apigatewayv2_route" "message" {
-    api_id = aws_apigatewayv2_api.backend.id
-    route_key = "$default"  
+  api_id    = aws_apigatewayv2_api.backend.id
+  route_key = "$default"
 }
 
 resource "aws_apigatewayv2_route" "disconnect" {
-    api_id = aws_apigatewayv2_api.backend.id
-    route_key = "$default"  
+  api_id    = aws_apigatewayv2_api.backend.id
+  route_key = "$default"
 }
 
 resource "aws_apigatewayv2_route_response" "connect" {
-  api_id = aws_apigatewayv2_api.backend.id
-  route_id = aws_apigatewayv2_route.connect.id
+  api_id             = aws_apigatewayv2_api.backend.id
+  route_id           = aws_apigatewayv2_route.connect.id
   route_response_key = "$default"
 }
 
 resource "aws_apigatewayv2_route_response" "message" {
-  api_id = aws_apigatewayv2_api.backend.id
-  route_id = aws_apigatewayv2_route.message.id
+  api_id             = aws_apigatewayv2_api.backend.id
+  route_id           = aws_apigatewayv2_route.message.id
   route_response_key = "$default"
 }
 
 resource "aws_apigatewayv2_route_response" "disconnect" {
-  api_id = aws_apigatewayv2_api.backend.id
-  route_id = aws_apigatewayv2_route.disconnect.id
+  api_id             = aws_apigatewayv2_api.backend.id
+  route_id           = aws_apigatewayv2_route.disconnect.id
   route_response_key = "$default"
 }
 
 resource "aws_apigatewayv2_stage" "backend" {
-    api_id = aws_apigatewayv2_api.backend.id
-    name = "backend-stage"  
+  api_id = aws_apigatewayv2_api.backend.id
+  name   = "backend-stage"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.backend.function_name
-    principal = "events.amazonaws.com"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.backend.function_name
+  principal     = "events.amazonaws.com"
 }
