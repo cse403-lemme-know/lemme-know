ba72f77739491c6e2029510468209847e05d0b13
Finn Bear <finnbearlabs@gmail.com>
2024-02-15 15:57:10 -0800

    Test cal mode parser.

--- backend/util_test.go
+++ backend/util_test.go
@@ -15,3 +15,18 @@ func TestTimeParsing(t *testing.T) {
 	assert.Nil(t, parseTime("sus"))
 	assert.NotNil(t, parseTime("19:10"))
 }
+
+func TestCalendarModeParsing(t *testing.T) {
+	start, end, dayOfWeek := parseCalendarMode("sus")
+	assert.Nil(t, start)
+	assert.Nil(t, end)
+	assert.False(t, dayOfWeek)
+	start, end, dayOfWeek = parseCalendarMode("dayOfWeek")
+	assert.Nil(t, start)
+	assert.Nil(t, end)
+	assert.True(t, dayOfWeek)
+	start, end, dayOfWeek = parseCalendarMode("2024-02-15 to 2024-03-04")
+	assert.NotNil(t, start)
+	assert.NotNil(t, end)
+	assert.False(t, dayOfWeek)
+}
